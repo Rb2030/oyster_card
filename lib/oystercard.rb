11bd981a948d70f00b7f83e4cc6c3d56e504b6d8
Rb2030 <rossmail2050@gmail.com>
2016-06-03 17:02:55 +0100

    Number 10, complete

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,10 +1,13 @@
 class Oystercard
-  attr_reader :balance
+  attr_reader :balance, :entry_station
 
 MAXIMUM = 90
+MINIMUM = 1
 
   def initialize
     @balance = 0
+    @in_journey = false
+    @entry_station = nil
   end
 
   def top_up(money)
@@ -12,19 +15,29 @@ MAXIMUM = 90
       @balance += money
   end
 
+  def touch_in(station)
+    raise 'Cannot touch in.' if balance < MINIMUM
+    @entry_station = entry_station
+    @in_journey = true
+  end
+
+  def touch_out
+    deduct(MINIMUM)
+    @in_journey = true
+    @in_journey = false
+  end
+
+  def in_journey?
+    false
+  end
+
   def deduct(fare)
     @balance -= fare
   end
 
-  def touch_in
-  end
-
-  def touch_out
-  end
-
-  def in_journey
-  end
 end
+
+
 
 # card = Oystercard.new
 # card.balance => nil
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,8 +1,15 @@
-require 'oystercard'
+ require 'oystercard'
 
 describe Oystercard do
   subject(:card) { described_class.new }
 
+  let(:station){ double :station }
+
+
+   it 'deducts an amount from the balance' do
+     card.top_up(20)
+     expect{ card.deduct 4 }.to change{card.balance}.by -4
+   end
 
   describe '#balance' do
     it "oystercard understands the balance method" do
@@ -17,6 +24,9 @@ describe Oystercard do
     end
     it 'has a maximum balance of 90' do
       expect(card.top_up(90)).to eq card.balance
+    end
+    it 'is initially not in a journey' do
+      expect(card).not_to be_in_journey
     end
 
   end
@@ -34,37 +44,47 @@ describe Oystercard do
     end
   end
 
-  describe '#deduct' do
-    it 'deducts 5 from the balance' do
-      p "#{card.balance}"
-      card.deduct(5)
-      p "#{card.deduct(5)}"
-      p "#{card.balance}"
-      expect(card.balance).to eq -5
-      p 'flang'
-    end
-    it 'deducts 10 from the balance' do
-      card.deduct(-10)
-      expect(card.balance).to eq -10
-    end
-  end
-
   describe '#touch_in' do
-    it 'The card is touched into a station' do
+
+    context 'when balance is above minimum' do
+    before do
+      card.top_up(20)
+    end
+    it 'It can touch in' do
       expect(card).to respond_to(:touch_in)
     end
+
+    it 'It can touch into a station' do
+      card.touch_in(station)
+      expect(card.touch_in(station)).to eq true
+    end
+    it 'stores the entry station' do
+      card.touch_in(station)
+      expect(card.touch_in(station)). to eq true
+      #expect(card.entry_station).to eq station
+    end
+  end
+    context 'when insufficient funds' do
+      it 'will not touch in, if below minimum balance' do
+      expect{card.touch_in(station)}.to raise_error 'Cannot touch in.'#expect{ card.touch_in }.to raise_error "Cannot touch in due to insufficient funds"
+    end
+  end
   end
 
   describe '#touch_out' do
     it 'the card has left a station' do
       expect(card).to respond_to(:touch_out)
     end
+    it ' deducts payment from card.' do
+    card.top_up(5)
+    card.touch_in(station)
+    expect{ card.touch_out }.to change{ card.balance }.by(-Oystercard::MINIMUM)
+    end
   end
 
   describe '#in_journey' do
     it 'the card is on a journey' do
-      expect(card).to respond_to(:in_journey)
+      expect(card).to respond_to(:in_journey?)
     end
   end
-
 end
